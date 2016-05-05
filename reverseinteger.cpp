5531aa293e74b09f6e7497b3e51130a3dc9f960e
David P Anderson <anderd7@bobby.CS.UNLV.EDU>
2016-05-05 14:08:36 -0700

    hw7

---
+++ reverseinteger.cpp
@@ -0,0 +1,21 @@
+/*David Anderson, CS 202 Assignment 7
+ *Description: A recursive function that takes in an integers and returns the number in reverse order as an integer.
+ *Input:  Any integer.
+ *Output:  The reverse of the input.
+ */
+
+#inlcude <iostream>
+
+using namespace std;
+
+int reverse(int);
+
+int main()
+{
+
+  return 0;
+}
+
+int reverse(int n)
+{
+}
